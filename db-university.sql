98915c863a03dcd07bbb2115d7f1e23971f15296
MrArcano <salvatore-dario@hotmail.it>
2023-11-13 16:39:08 +0100

    [edit]: query

--- db-university.sql
+++ db-university.sql
@@ -1,31 +1,48 @@
---1
+--Query con SELECT
+--1. Selezionare tutti gli studenti nati nel 1990 (160)
 SELECT *
 FROM `students` 
 WHERE YEAR(`date_of_birth`) = 1990;
 	
---2
+--2. Selezionare tutti i corsi che valgono più di 10 crediti (479)
 SELECT * 
 FROM `courses` 
 WHERE `cfu` > 10  
 
---3
--- non mi convince
+--3. Selezionare tutti gli studenti che hanno più di 30 anni
+-- non mi convince?? DATEDIFF
 SELECT * 
 FROM `students` 
 WHERE YEAR(CURRENT_DATE) - YEAR(`date_of_birth`) > 30
 
---4
+--4. Selezionare tutti i corsi del primo semestre del primo anno di un qualsiasi corso di laurea (286)
 SELECT * 
 FROM `courses` 
 WHERE `period` = 'I semestre' AND `year` = 1
 
---5
+--5. Selezionare tutti gli appelli d'esame che avvengono nel pomeriggio (dopo le 14) del 20/06/2020 (21)
 SELECT * 
 FROM `exams` 
 WHERE `date` = '2020-06-20' AND `hour` > '14:00:00';
 
---6
+--6. Selezionare tutti i corsi di laurea magistrale (38)
+SELECT * 
+FROM `degrees` 
+WHERE `level` = 'magistrale'
 
---7
+--7. Da quanti dipartimenti è composta l'università? (12)
+SELECT COUNT(*) AS `num_departments` 
+FROM `departments`
 
---8
+--8. Quanti sono gli insegnanti che non hanno un numero di telefono? (50)
+SELECT COUNT(*) AS `teachers_no_phone`
+FROM `teachers` 
+WHERE `phone` IS NULL
+
+
+
+--Query con GROUP BY
+--1. Contare quanti iscritti ci sono stati ogni anno
+--2. Contare gli insegnanti che hanno l'ufficio nello stesso edificio
+--3. Calcolare la media dei voti di ogni appello d'esame
+--4. Contare quanti corsi di laurea ci sono per ogni dipartimento
